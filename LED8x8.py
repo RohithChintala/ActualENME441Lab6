e140aa0621f6e74911af27d273ece27f7ee51e99
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-17 20:34:35 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -1,12 +1,39 @@
 import time
 from shifter import Shifter    # extend by composition
 import multiprocessing
+import random
 
 class LED8x8(multiprocessing.Process):
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
-    multiprocessing.Process.__init__(self)
-  def display(self, num, a):
-    self.shifter.shiftByte(a[num])
+    b = multiprocessing.Array('i',8)
+    ay = 5
+    ax = 5
+    g = 0b1
+    mask = 0b11111111 
+    while True:
+      h = 0
+      while h == 0:
+        x = random.randint(-1, 1)
+        y = random.randint(-1, 1)
+        ax += x
+        ay += y
+        if ax >= 0:
+          if ax <= 7:
+            if ay >= 0:
+              if ay <= 7:
+                h = 1
+        else:
+          h = 0
+          ax -= x
+          ay -= y
+      f = g << abs(8-ax)
+      e = ~f & mask
+      b[ay] = e
+      self.p = multiprocessing.Process(name='myname', target = self.display(ay, b), args = (ay, b))
+      self.p.daemon = True
+      self.p.start()
+      time.sleep(0.1)
+  def display(self, num, c):
+    self.shifter.shiftByte(c[num])
     self.shifter.shiftByte(1 << (num))
-
--- lab6.py
+++ lab6.py
@@ -1,8 +1,7 @@
 import time
 from LED8x8 import LED8x8
-from LED8x8Copy import LED8x8Copy
 import multiprocessing
 import random
 
 dataPin, latchPin, clockPin = 13, 19, 26
-LED = LED8x8Copy(dataPin, latchPin, clockPin)
+LED = LED8x8(dataPin, latchPin, clockPin)

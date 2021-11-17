8adf8a0ac11efae861fd4069f4c8a0f3ca5ea2e8
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-17 01:32:58 +0000

    d

--- LED8x8Copy.py
+++ LED8x8Copy.py
@@ -8,7 +8,7 @@ class LED8x8Copy(multiprocessing.Process):
     self.b = multiprocessing.Array('i',8)
     self.b[num] = a
     #multiprocessing.Process.__init__(self, target=self.display(num,a), args= (num, a))
-    self.p = multiprocessing.Process(name='myname', target = self.display(num, a), args = (num, a))
+    self.p = multiprocessing.Process(name='myname', target = self.display(num, self.b), args = (num, self.b))
     self.p.daemon = True
     self.p.start()
     #self.p.join()
@@ -19,7 +19,7 @@ class LED8x8Copy(multiprocessing.Process):
       #self.p = multiprocessing.Process(name='myname', target = self.display(num, a), args = (num, a))
       #self.p.daemon = True
       #self.p.join()
-  def display(self, num, a):
+  def display(self, num, c):
     while True:
-      self.shifter.shiftByte(self.b[num])
+      self.shifter.shiftByte(c[num])
       self.shifter.shiftByte(1 << (num))

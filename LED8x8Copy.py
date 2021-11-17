f8ffcefca692867672d8b8af962802ddc3665721
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-17 00:25:56 +0000

    d

--- LED8x8Copy.py
+++ LED8x8Copy.py
@@ -3,12 +3,12 @@ from shifter import Shifter    # extend by composition
 import multiprocessing
 
 class LED8x8Copy(multiprocessing.Process):
-  def __init__(self, data, latch, clock, num, b):
+  def __init__(self, data, latch, clock, num, a):
     #self.shifter = Shifter(data, latch, clock)
     #multiprocessing.Process.__init__(self, target=self.display)
     while True:
       self.b = multiprocessing.Array('i',8)
-      self.b = b
+      self.b = a
       self.shifter = Shifter(data, latch, clock)
       #self.b = multiprocessing.Array('i',8)
       #self.b[num] = a[num]
